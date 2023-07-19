ad03b1f2337fd6aa2ff5114d2c6e078879379cfb
isabelavitoria <isabelavitoria3005@gmail.com>
2023-07-19 17:54:15 -0300

    nb

---
+++ my-project/resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{-- Nothing in the world is as soft and yielding as water. --}}
+</div>
+<div style="text-align: center">
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+</div>
